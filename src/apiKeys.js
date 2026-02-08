51c75e50fbd1a2541c404501154b94ad88ad4d25
dipa180 <dipanwitap06@gmail.com>
2026-02-08 17:19:20 +0530

    updated

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,10 +1,6 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-// module.exports = {
-//   key: "b7632e8a87b1d60a407020081e118e76",
-//   base: "https://api.openweathermap.org/data/2.5/",
-// };
+
 const apiKeys = {
-  key: "84de4a4d505b968c5e8cd1e8690b116f",
+  key: "",
   base: "https://api.openweathermap.org/data/2.5/"
 };
 
